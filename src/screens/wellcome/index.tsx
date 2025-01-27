05b1964e2398c41f03ba932a20906d508e192467
joelmirmanoel <joelmirsilva80@outlook.com>
2025-01-26 23:31:39 -0300

    finalizando o  primeiro dia

--- src/screens/wellcome/index.tsx
+++ src/screens/wellcome/index.tsx
@@ -1,11 +1,30 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { styles } from "./styles";
+import React from 'react';
+import {Dimensions, Text, TouchableOpacity, View} from 'react-native';
+import {styles} from './styles';
+import Brand from '../../assets/images/cloudAndThunder.svg';
+
+const {width, height} = Dimensions.get('window');
 
 export function WellcomeScrreen() {
-    return (
-        <View style={styles.Container}>
-            <Text style={styles.Title}>Hello word</Text>
-        </View>
-    );
+  const percentageWidth = 0.61 * width;
+  const percentageHeight = 0.26 * height;
+
+  return (
+    <View style={styles.Container}>
+      <Brand
+        style={styles.image}
+        width={percentageWidth}
+        height={percentageHeight}
+      />
+      <Text style={styles.Title}>Descubra o Clima na sua Cidade </Text>
+      <Text style={styles.Content}>
+        Com o <Text style={styles.contentBold}>FindWeather</Text> nunca ficou
+        tão fácil ter a previsão do tempo na palma da sua mão
+      </Text>
+
+      <TouchableOpacity style={styles.Button}>
+        <Text style={styles.contentButton}>Iniciar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
--- src/screens/wellcome/styles.ts
+++ src/screens/wellcome/styles.ts
@@ -1,15 +1,57 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
+import {Theme} from '../../theme/styles';
 
 export const styles = StyleSheet.create({
-    Container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+  Container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Theme.colors.dark,
+    paddingHorizontal: 20,
+  },
 
-    },
+  image: {
+    marginTop: 40,
+    marginBottom: 60,
+  },
 
-    Title: {
-        fontSize: 40,
-        color: 'blue',
-    },
+  Title: {
+    fontSize: 33,
+    color: Theme.colors.white,
+    fontFamily: 'Overpass Bold',
+    textAlign: 'center',
+  },
+
+  Content: {
+    color: Theme.colors.gray100,
+    fontSize: 22,
+    marginTop: 30,
+    paddingTop: 20,
+    paddingBottom: 80,
+    textAlign: 'center',
+    fontFamily: 'Overpass Regular',
+  },
+
+  contentBold: {
+    fontFamily: 'Overpass Bold',
+    borderLeftColor: Theme.colors.white,
+  },
+
+  Button: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Theme.colors.dark300,
+    borderColor: Theme.colors.gray300,
+    borderWidth: 1,
+    borderRadius: 18,
+    padding: 12,
+  },
+
+  contentButton: {
+    color: Theme.colors.white,
+    fontSize: 22,
+    fontFamily: 'Overpass Regular',
+    alignItems: 'center',
+  },
 });
